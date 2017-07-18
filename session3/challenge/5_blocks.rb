66e884ae0d706507ed0e7dae2552f7b70bb2e476
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-07-18 02:32:31 +0100

    session3 challenges 5 & 6

--- session3/challenge/5_blocks.rb
+++ session3/challenge/5_blocks.rb
@@ -16,3 +16,91 @@
 #   order << i
 # end
 # order # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+def spiral_access(arrays, iteration=0, &block)
+  y_max = arrays.length - 1
+  x_max = arrays.first.length - 1
+
+  # base step
+  return if y_max/2 < iteration || x_max/2 < iteration
+
+  # top row
+  iteration.upto x_max-iteration do |x|
+    block.call arrays[iteration][x]
+  end
+
+  # right column
+  (iteration + 1).upto y_max-iteration do |y|
+    block.call arrays[y][x_max-iteration]
+  end
+
+  # bottom row
+  (x_max - 1 - iteration).downto iteration do |x|
+    block.call arrays[y_max-iteration][x]
+  end
+
+  # left column
+  (y_max - 1 - iteration).downto iteration+1 do |y|
+    block.call arrays[y][iteration]
+  end
+
+  # recursive step
+  spiral_access arrays, iteration+1, &block
+end
+
+
+
+=begin
+
+  height=[]
+  h=2d_array.length-1
+  (0..h).each{|x| height<<x}
+
+  weight=[]
+  w=2d_array[0].length-1
+  (0..h).each{|x| weight<<x}
+
+  two_d[0][0]
+  two_d[0][1]
+  two_d[0][2]
+  two_d[0][3]
+  two_d[0][4]
+  two_d[1][4]
+  two_d[2][4]
+  two_d[3][4]
+  two_d[4][4]
+  two_d[4][3]
+  two_d[4][2]
+  two_d[4][1]
+  two_d[4][0]
+  two_d[3][0]
+  two_d[2][0]
+  two_d[1][0]
+  two_d[1][1]
+  two_d[1][2]
+  two_d[1][3]
+  two_d[2][3]
+  two_d[3][3]
+  two_d[3][2]
+  two_d[3][1]
+  two_d[2][1]
+  two_d[2][2]
+
+
+two_d = [
+[ 1,  2,  3,  4, 5],
+[16, 17, 18, 19, 6],
+[15, 24, 25, 20, 7],
+[14, 23, 22, 21, 8],
+[13, 12, 11, 10, 9],
+]
+two_d.size
+two_d.length
+two_d[0][1]
+two_d2 = [
+[ 1,  2,  3, 4],
+[16, 17, 18, 2],
+[15, 24, 25, 1],
+]
+two_d2[0].length
+
+=end
--- session3/challenge/6_arguments.rb
+++ session3/challenge/6_arguments.rb
@@ -17,3 +17,115 @@
 # match_maker true,  true,  true, true, nil     # => [false, true]
 # match_maker true,  true,  true, 0, nil        # => [false, true]
 
+def match_maker(opposites_attract, *elements)
+  to_return = []
+  elements.each_slice 2 do |first, last|
+    first  = !!first
+    last   = !!last
+    result = if opposites_attract
+               first != last
+             else
+               first == last
+             end
+    to_return << result
+  end
+  to_return
+end
+
+=begin
+
+identifier=true
+bool_array=[true, false]
+bool_array.slice(0..bool_array.length-2)
+bool_array.length
+bool_array[0]
+bool_array[bool_array.length-1]
+match_maker(identifier, [true, true, false])
+match_maker(true, true, false)
+
+match_maker(true, bool_array.slice(0..bool_array.length-2))
+bool_array[bool_array.length-1]
+bool_array[0]
+match_maker true,  true,  true, 0, nil
+
+def match_maker(identifier, n=0, *bool_array)
+  arr=[]
+  if identifier==true
+    if bool_array[0]!=bool_array[1]
+      arr << true
+    else
+      arr<<false
+    end
+    loop{
+    break if n>=bool_array.length-2
+    n+=2
+    if bool_array[n]!=bool_array[n+1]
+      arr << true
+    else
+      arr<<false
+    end
+    }
+  else
+    if bool_array[0]==bool_array[1]
+      arr << true
+    else
+      arr<<false
+    end
+    loop{
+    break if n>=bool_array.length-2
+    n+=2
+    if bool_array[n]==bool_array[n+1]
+      arr << true
+    else
+      arr<<false
+    end
+    }
+  end
+  arr
+end
+
+match_maker true,  true,  true, 0, nil
+
+
+  arr << true if bool_array[2]!=bool_array[3] else arr<<false
+  arr << true if bool_array[4]!=bool_array[5] else arr<<false
+
+
+
+  arr << true if bool_array[0]==bool_array[1] && identifier==false && bool_array.length==2
+
+  arr << true if bool_array[0]!=bool_array[1] && identifier==true && bool_array.length==4
+  arr << true if bool_array[0]==bool_array[1] && identifier==false && bool_array.length==4
+
+  arr << true if bool_array[bool_array.length-2]!=bool_array[bool_array.length-1]&& identifier==true && bool_array.length==4
+  arr << true if bool_array[bool_array.length-2]==bool_array[bool_array.length-1]&& identifier==false && bool_array.length==4
+
+  else
+  arr << false
+  end
+
+arr
+
+end
+
+
+
+return true if match_maker(bool_array.slice(0..1))!=bool_array[2] if bool_array.length==3
+return true if match_maker(bool_array.slice(0..2))!=bool_array[3] if bool_array.length==4
+
+
+
+if bool_array.length>2
+
+arr << true if match_maker(identifier, bool_array.slice(0..bool_array.length-2))!=bool_array[bool_array.length-1] && identifier==true || match_maker(identifier, bool_array.slice(0..bool_array.length-2))==bool_array[bool_array.length-1] && identifier==false
+else
+arr << false
+end
+
+
+
+  arr << true if match_maker(identifier,bool_array.slice(0..bool_array.length-2))!=bool_array[bool_array.length-1] && identifier==true || match_maker(identifier, bool_array.slice(0..bool_array.length-2))==bool_array[bool_array.length-1] && identifier==false
+  else
+  arr << false
+  end
+=end
